d80baf1b585bf65f3d2aa7d4ea740b0d18a31ff8
darienmiller88 <darienmiller88@yahoo.com>
2024-09-30 16:29:00 -0400

    finally got htmx working! Can now append users to a list

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"html/template"
+	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main(){
@@ -16,7 +17,9 @@ func main(){
 	router := chi.NewRouter()
 	fs := http.FileServer(http.Dir("./static"))
 	tmpl := template.Must(template.ParseGlob("./templates/*"))
+	users := []string{"Darien", "Vicky", "Richard", "Kash"}
 
+	router.Use(middleware.Logger, middleware.RealIP, middleware.Recoverer)
     router.Handle("/static/*", http.StripPrefix("/static/", fs))
 
 	router.Get("/hello", func(res http.ResponseWriter, req *http.Request) {
@@ -25,8 +28,27 @@ func main(){
 
 	router.Get("/", func(res http.ResponseWriter, req *http.Request) {
 		tmpl.ExecuteTemplate(res, "scoreboard.html", map[string][]string{
-			"Users": {"Darien", "Vicky", "Richard", "Kash"},
+			"Users": users,
 		})
+	})
+
+	router.Post("/", func(res http.ResponseWriter, req *http.Request) {
+		if err := req.ParseForm(); err != nil{
+			http.Error(res, "Invalid form data", http.StatusBadRequest)
+            return
+		}
+
+		newUser := req.FormValue("addUser")
+
+		if newUser == "" {
+			http.Error(res, "User cannot be empty.", http.StatusBadRequest)
+			return
+		}
+
+		users = append(users, newUser)
+
+		//Execute a block element in the html file to add a new user to the "users" ul element.
+		tmpl.ExecuteTemplate(res, "user-element", newUser)
 	})
 
 	fmt.Println("listening on port:", os.Getenv("PORT"))
