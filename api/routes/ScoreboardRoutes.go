59f1fc69354eee95d4371743ce2b235c95ec71f3
darienmiller88 <32966645+darienmiller88@users.noreply.github.com>
2024-11-01 01:41:07 -0400

    Update and rename scoreboardRoutes.go to ScoreboardRoutes.go

--- api/routes/ScoreboardRoutes.go
+++ api/routes/ScoreboardRoutes.go
@@ -10,17 +10,17 @@ type ScoreBoardRoutes struct{
 	Router *chi.Mux
 }
 
-
 func (s *ScoreBoardRoutes) Init(){
 	s.Router = chi.NewRouter()
 
 	s.Router.Get("/get-location/{location-name}", controllers.GetLocation)
-	s.Router.Get("/get-all-locations", controllers.GetAllLocations)
-	s.Router.Get("/get-all-users/{location-name}", controllers.GetAllUsersByLocation)
-	s.Router.Get("/get-all-users", controllers.GetAllUsers)
-	s.Router.Get("/get-saved-games", controllers.GetAllSavedGames)
 	s.Router.Get("/get-saved-games/{location-name}", controllers.GetAllSavedGames)
+	s.Router.Get("/get-all-users/{location-name}", controllers.GetAllUsersByLocation)
+	s.Router.Get("/get-all-locations", controllers.GetAllLocations)
+	s.Router.Get("/get-saved-games", controllers.GetAllSavedGames)
+	// s.Router.Get("/get-all-users", controllers.GetAllUsers)
+	// s.Router.Post("/add-location", controllers.AddLocation)
 	s.Router.Post("/save-game", controllers.SaveGame)
-	s.Router.Post("/add-user-to-location", controllers.AddUserToLocation)
-	s.Router.Delete("/remove-user-from-location", controllers.RemoveUserFromLocation)
+	s.Router.Post("/add-user-to-location/{location-name}", controllers.AddUserToLocation)
+	s.Router.Delete("/remove-user-from-location/{location-name}/{username}", controllers.RemoveUserFromLocation)
 }
