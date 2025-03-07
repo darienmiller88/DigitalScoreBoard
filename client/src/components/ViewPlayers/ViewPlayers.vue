682e392eaead209f75826b1577b70d02cafbb066
darienmiller88 <darienmiller88@yahoo.com>
2025-03-07 10:03:33 -0500

    view players styling added

--- client/src/components/ViewPlayers/ViewPlayers.vue
+++ client/src/components/ViewPlayers/ViewPlayers.vue
@@ -5,11 +5,31 @@
 </script>
 
 <template>
-    <ul v-for="(player, index) in props.teamPlayers">
+    <!-- <ul v-for="(player, index) in props.teamPlayers">
          <li :key="index">{{ player }}</li>
-    </ul>
+    </ul> -->
+    <div class="players">
+        <div class="player" v-for="(player, index) in props.teamPlayers" :key="index">
+            {{ player }}
+        </div>
+    </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+    .players{
+        text-align: center;
+        // border: 2px solid red;
+        
+        max-height: 25vh;
+        overflow-y: scroll;
 
+        .player{
+            margin: 10px;
+            font-size: 18px;
+
+            @media screen and (min-width: 992px) {
+                font-size: 26px;
+            }
+        }
+    }
 </style>
