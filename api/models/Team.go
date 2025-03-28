f59b73fc7beec10bf9dc9bf76583f6800e751448
darienmiller88 <darienmiller88@yahoo.com>
2025-03-27 20:28:49 -0400

    adjusting Team validation in Team.go

--- api/models/Team.go
+++ api/models/Team.go
@@ -1,5 +1,9 @@
 package models
 
+// import(
+// 	"github.com/go-ozzo/ozzo-validation"
+// )
+
 type Team struct{
 	Score     int      `bson:"score"      json:"score"` 
 	TeamName  string   `bson:"team_name"  json:"team_name"`
