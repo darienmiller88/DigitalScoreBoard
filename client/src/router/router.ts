66a0df9c635b2004fc9a311b3ac0bf45f61a1cbb
unknown <darienmiller88@yahoo.com>
2025-05-25 22:49:54 -0400

    feat: added new routes to vue router

--- client/src/router/router.ts
+++ client/src/router/router.ts
@@ -3,10 +3,12 @@ import Home from "../pages/Home/Home.vue"
 import Register from '../pages/Register/Register.vue'
 import GenerateJeopardyQuestions from '../pages/GenerateJeopardyQuestions/GenerateJeopardyQuestions.vue'
 import Games from '../pages/Games/Games.vue'
+import TournamentMode from '../pages/TournamentMode/TournamentMode.vue'
+// import AddNewUser from '../pages/AddNewUser/AddNewUser.vue'
 
 // Create the router instance
 const router = createRouter({
-    history: createWebHistory(), // Uses HTML5 history mode
+    history: createWebHistory(), 
     routes: [
         {
             path: "/",
@@ -23,6 +25,10 @@ const router = createRouter({
         {
             path: "/generate-questions",
             component: GenerateJeopardyQuestions
+        },
+        {
+            path: "/tournament-mode",
+            component: TournamentMode
         }
     ]
 })
