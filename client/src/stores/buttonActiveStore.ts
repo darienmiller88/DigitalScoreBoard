ff2c62a09e5756a2bd0e99ddd3c39e11f0b571f6
unknown <darienmiller88@yahoo.com>
2025-05-27 00:20:36 -0400

    feat: preparing buttonActiveStore,ts ot be changed to gameModeStore, adding necessary information

--- client/src/stores/buttonActiveStore.ts
+++ client/src/stores/buttonActiveStore.ts
@@ -7,6 +7,24 @@ export enum ButtonState {
     CREATE_NEW_TEAM_GAME = 2,
 }
 
+export enum GameMode {
+    CREATE_NEW_GAME = 0,
+    ADD_NEW_USER = 1,
+    CREATE_NEW_TEAM_GAME = 2,
+}
+
+export const gameModeStore = defineStore("game-mode", () => {
+    const currentGameMode = ref<GameMode>(GameMode.ADD_NEW_USER)
+
+    const setGameMode = (state: GameMode) => {
+        currentGameMode.value = state
+    }
+
+    return { setGameMode, currentGameMode }
+}, {
+    persist: true
+})
+
 export const buttonActiveStore = defineStore("button-active", () => {
     const currentButtonGroupState = ref<ButtonState>(ButtonState.ADD_NEW_USER)
 
