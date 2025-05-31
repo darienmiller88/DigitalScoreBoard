16731ee450689a19f0b8ca9c34532df62a2d1f30
unknown <darienmiller88@yahoo.com>
2025-05-31 12:02:00 -0400

    feat: added page title component for each page to use

---
+++ client/src/components/PageTitle/PageTitle.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    const props = defineProps<{
+        titleName: string
+    }>()
+</script>
+
+<template>
+    <div class="title">{{ props.titleName }}</div>
+</template>
+
+<style scoped lang="scss">
+    .title{
+        color: var(--main-text-color);
+        text-align: center;
+        font-size: 40px;
+        margin: 40px 0px;
+
+        @media screen and (min-width: 768px) {
+            font-size: 60px;
+            margin: 10px 0px;
+        }
+    }
+</style>
