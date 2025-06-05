672c04ca59e6eba457f4bacdcf81f2eb7e64220a
unknown <darienmiller88@yahoo.com>
2025-06-05 01:52:55 -0400

    feat: added some styling to teams in ViewSavedGameTeams.vue, scrolling is now active

--- client/src/components/ViewSavedGameTeams/ViewSavedGameTeams.vue
+++ client/src/components/ViewSavedGameTeams/ViewSavedGameTeams.vue
@@ -9,7 +9,9 @@
 <template>
     <div class="teams"> 
         <div class="team" v-for="team in props.teamsInSavedGame">
-            {{ team.team_name }}
+            <div class="team-name">
+                {{ team.team_name }}
+            </div> 
 
             <div class="players">
                 <div class="player" v-for="player in team.players">
@@ -22,16 +24,28 @@
 
 <style scoped lang="scss">
     .teams{
+        text-align: center;
         border: 2px solid black;
+        height: 30vh;
+        overflow-y: scroll;
 
         .team{
             border: 2px solid red;
 
+            .team-name{ 
+                font-size: 25px;
+                font-weight: 700;
+                padding: 20px;
+            }
+
             .players{
                 border: 2px solid purple;
-
+                height: 13vh;
+                overflow-y: scroll;
+                
                 .player{
-
+                    font-size: 18px;
+                    padding: 10px;
                 }
             }
         }
