3d582c45dde03b389c88875ebf8c3ec10bdf2be1
unknown <darienmiller88@yahoo.com>
2025-06-15 15:58:13 -0400

    style: added grid templates to usercardscontianer.vue

--- client/src/container/UserCardsContainer/UserCardsContainer.vue
+++ client/src/container/UserCardsContainer/UserCardsContainer.vue
@@ -20,6 +20,25 @@
 
 <style scoped lang="scss">
     .user-cards{
+        display: grid;
+        grid-template-columns: 1fr;    
+
+        width: fit-content; 
+        max-height: 50vh;
+        max-width: 90vw;
+
+        margin: auto;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        overflow-y: scroll;
+
         border: 2px solid black;
+
+        @media only screen and (min-width: 768px){
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 30px;
+            max-height: 75vh;
+        }
     }
 </style>
