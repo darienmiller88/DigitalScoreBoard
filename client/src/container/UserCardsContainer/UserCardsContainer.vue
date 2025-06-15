3ee60ca70da5273e522ac2eb9b65f307d1852d5e
unknown <darienmiller88@yahoo.com>
2025-06-15 16:00:59 -0400

    style: added media query for small laptops and larger tablets with resolution of at least 992px across

--- client/src/container/UserCardsContainer/UserCardsContainer.vue
+++ client/src/container/UserCardsContainer/UserCardsContainer.vue
@@ -34,11 +34,22 @@
 
         border: 2px solid black;
 
+        // Show two columns of cards for tablets
         @media only screen and (min-width: 768px){
             display: grid;
             grid-template-columns: repeat(2, 1fr);
             gap: 30px;
             max-height: 75vh;
         }
+
+        //Show three columns for laptops less than 2K
+        @media only screen and (min-width: 992px){
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 30px;
+            max-height: 75vh;
+        }
+
+       
     }
 </style>
