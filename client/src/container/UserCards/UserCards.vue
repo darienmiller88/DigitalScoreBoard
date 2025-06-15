0b2ce690875997770afa6427ff0cce5b1f226968
unknown <darienmiller88@yahoo.com>
2025-06-15 11:41:18 -0400

    feat: Added UserCard.vue

---
+++ client/src/container/UserCards/UserCards.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    import UserCard from '../../components/UserCard/UserCard.vue';
+</script>
+
+<template>
+    <UserCard />
+</template>
+
+<style scoped lang="scss">
+
+</style>
