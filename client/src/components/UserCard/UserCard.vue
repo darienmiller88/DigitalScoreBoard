b3aff3b6f2cdf112c5abc9dbb72cf99310bbe5ba
unknown <darienmiller88@yahoo.com>
2025-06-15 15:47:26 -0400

    fix: adding styling for UserCard and userCards

--- client/src/components/UserCard/UserCard.vue
+++ client/src/components/UserCard/UserCard.vue
@@ -1,13 +1,40 @@
 <script setup lang="ts">
-
+    defineProps<{
+        username: string
+    }>()
 </script>
 
 <template>
-    <div>
-
+    <div class="user-card">
+        <div class="username">{{ username }}</div>
     </div>
 </template>
 
 <style scoped lang="scss">
+    .user-card{
+        border: 2px solid var(--primary-color);
+        border-radius: 10px;
 
+        width: 90vw;
+        box-shadow: 8px 8px 5px rgba(173, 216, 230, 0.548);
+        margin: 20px 0px; 
+
+        transition: 0.3s;
+        text-align: center;
+
+        @media screen and (min-width: 768px) {
+            width: 40vw;
+            margin: 20px 0px; 
+        }
+
+        @media screen and (min-width: 992px) {
+            width: 25vw;
+            margin: 20px 0px; 
+        }
+
+        @media screen and (min-width: 1400px) {
+            width: 15vw;
+            margin: 20px 0px; 
+        }
+    }
 </style>
--- client/src/container/UserCardsContainer/UserCardsContainer.vue
+++ client/src/container/UserCardsContainer/UserCardsContainer.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
     import UserCard from '../../components/UserCard/UserCard.vue';
+
+    const usernames = [
+        "darien miller",
+        "angela lourens",
+        "mark rubens",
+        "shaniqua blank"
+    ]
 </script>
 
 <template>
-    <UserCard />
+    <div class="user-cards">
+        <UserCard
+            v-for="username in usernames"
+            :username="username"
+        />
+    </div>
 </template>
 
 <style scoped lang="scss">
-
+    .user-cards{
+        border: 2px solid black;
+    }
 </style>
