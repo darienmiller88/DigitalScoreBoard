93186aeffb7952ae64325de30725a2f511bb5161
unknown <darienmiller88@yahoo.com>
2025-06-19 18:37:50 -0400

    style: added tags to input in Edit PLayer Name modal

--- client/src/components/EditPlayerName/EditPlayerName.vue
+++ client/src/components/EditPlayerName/EditPlayerName.vue
@@ -1,16 +1,40 @@
 <script setup lang="ts">
+    import { ref } from 'vue';
+
     defineProps<{
         username: string
     }>()
+
+    let editFirstName = ref<string>("")
+    let editLastName = ref<string>("")
+
+    const onSubmit = () => {
+        console.log(editFirstName, editLastName)
+    }
 </script>
 
 <template>
     <div>
-        Editing {{ username }}
+        Editing player "{{ username }}"...
     </div>
-    <form @submit.prevent="">
-        <input type="text" placeholder="Edit First Name">
-        <input type="text" placeholder="Edit Last Name"/>
+    <form @submit.prevent="onSubmit">
+        <input 
+            class="form-element"
+            id="edit-first-name"
+            v-model="editFirstName"
+            name="edit-first-name"
+            type="text" 
+            placeholder="Edit First Name"
+        /><br />
+        <input 
+            class="form-element"
+            id="edit-last-name"
+            v-model="editLastName"
+            name="edit-last-name"
+            type="text" 
+            placeholder="Edit Last Name"
+        /><br />
+        <button>Submit</button>
     </form>
 </template>
 
