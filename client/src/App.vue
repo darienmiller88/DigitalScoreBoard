2fdd562d851ebd28ac9c4a6897dc9d83b1f74683
unknown <darienmiller88@yahoo.com>
2025-08-05 16:01:42 -0400

    chore: debugging by showing all location loaded in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,9 +2,16 @@
   import { onMounted } from "vue";
   import NavBar from "./components/NavBar/NavBar.vue"
   import { getAllLocations } from "./utils/utils"
+  import { optionsStore } from "./stores/optionsStore"
+  import { storeToRefs } from "pinia";
+  
+  const { allLocationOptions } = storeToRefs(optionsStore())
 
   onMounted(async () => {
       await getAllLocations()
+
+      console.log("locations:", allLocationOptions.value);
+      
   })
 </script>
 
