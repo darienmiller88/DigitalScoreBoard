9b60b65eae2231d80b4c3a6509631407b22a9b4b
unknown <darienmiller88@yahoo.com>
2025-08-06 02:16:04 -0400

    fix: moved initial loading of location data into main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia';
+import { getAllLocations } from "./utils/utils"
+
 import Toast from "vue-toastification";
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import "vue-toastification/dist/index.css";
-
 
 import App from './App.vue'
 import router from './router/router'
@@ -16,5 +17,7 @@ pinia.use(piniaPluginPersistedstate)
 app.use(Toast);
 app.use(pinia)
 app.use(router)
+
+await getAllLocations()
 
 app.mount('#app')
