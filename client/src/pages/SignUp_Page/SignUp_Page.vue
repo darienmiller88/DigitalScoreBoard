937cbe25f0ce10c5653dcab74750bca5bac82f9c
unknown <darienmiller88@yahoo.com>
2025-08-12 22:16:05 -0400

    fix: moved adapt logo from pulic folder to assets

--- client/src/pages/SignUp_Page/SignUp_Page.vue
+++ client/src/pages/SignUp_Page/SignUp_Page.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
     import { ref } from "vue";
-    import adaptLogo from "../../../public/adapt_logo-nobg.png"
+    import adaptLogo from "../../assets/adapt_logo-nobg.png"
 
     const isSignUpForm = ref<boolean>(true)
 
@@ -13,14 +13,14 @@
     <div class="signup">
         <form v-if="isSignUpForm" class="signup-form" @submit.prevent="handleSubmit">
             <img :src="adaptLogo" alt="">
-            <div>Sign up form</div>
+            <div class="header">Sign up form</div>
 
             <button @click="() => isSignUpForm = false">log in</button>
             <button @click="() => $router.push('/')"> continue to home</button>
         </form>
         <form v-else class="login-form" @submit.prevent="handleSubmit">
             <img :src="adaptLogo" alt="">
-            <div>Login form</div>
+            <div class="header">Login form</div>
 
             <button @click="() => isSignUpForm = true">log in</button>
             <button @click="() => $router.push('/')">continue to home</button>
@@ -39,16 +39,20 @@
         width: 100vw;
         height: 100vh;
         background-color: #003366;
-    }
+    
+        form{
+            background-color: #344966;
+            padding-left: 30px;
+            padding-right: 100px;
+    
+            img{
+                width: 200px;
+                height: auto;
+            }
 
-    form{
-        background-color: #344966;
-        padding-left: 30px;
-        padding-right: 100px;
-
-        img{
-            width: 200px;
-            height: auto;
+            .header{
+                font-size: 25px;
+            }
         }
     }
 </style>
