100c861d69cdbaaccdd8a8a05f2bc29884f9b330
Darien Miller <darienmiller88@yahoo.com>
2025-09-06 22:52:43 -0400

    fix: changed api link to railway, sadly had to drop 5 dollars lol

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -4,13 +4,13 @@ const userApiURL =   window.location.hostname === "localhost" || window.location
 ? 
 "http://localhost:8000/api/v1/users" 
 :
-"https://adaptscoreboard-678166011633.us-east4.run.app/api/v1/users"
+"https://digitalscoreboard.up.railway.app/api/v1/users"
 
 const scoreBoardApiUrl =  window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
 ?
 "http://localhost:8000/api/v1/scoreboard"
 :
-"https://adaptscoreboard-678166011633.us-east4.run.app/api/v1/scoreboard"
+"https://digitalscoreboard.up.railway.app/api/v1/scoreboard"
 
 export const userApi = axios.create({
     baseURL: userApiURL,
