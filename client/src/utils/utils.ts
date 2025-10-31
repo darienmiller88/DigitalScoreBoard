f5b902138f59802ce619c61f0b20382f7343c2da
Darien Miller <darienmiller88@yahoo.com>
2025-10-31 16:11:42 -0400

    progress: in utils, moved retrieval of locations into if statement to be read only once

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -5,25 +5,21 @@ import { Location } from "../types/types"
 
 export const getAllLocations = async () => {
     const { setAllLocationOptions } = optionsStore()
-    const { allLocationOptions, allLocationOptionsForTournament } = storeToRefs(optionsStore())
+    const { allLocationOptions } = storeToRefs(optionsStore())
 
     //Only load the all the location names if they're NOT in location storage. As a result, this function will be called
     //most one time. This is done to reduce database calls.
     if (!allLocationOptions.value.length) {
-    
-    
-    }
-    
-    
-    try {
-        const locationsResponse = await scoreBoardApi.get<Location[]>("/get-all-locations")
+        try {
+            const locationsResponse = await scoreBoardApi.get<Location[]>("/get-all-locations")
+                
+            //Load all the locations into the following ref, storing it into local storage for faster access.
+            setAllLocationOptions(locationsResponse.data.map(location => {          
+                return location.location_name
+            }))
             
-        //Load all the locations into the following ref, storing it into local storage for faster access.
-        setAllLocationOptions(locationsResponse.data.map(location => {          
-            return location.location_name
-        }))
-        
-    } catch (error) {
-        console.log("err:", error);
+        } catch (error) {
+            console.log("err:", error);
+        }
     }
 }
